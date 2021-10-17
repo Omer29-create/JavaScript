2f828dc0bbf40eca2528b15cfa41add2e964029f
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2021-10-17 22:02:07 +0200

    Create script.js

---
+++ Tag 1/script.js
@@ -0,0 +1,56 @@
+"use strict";
+
+// einzeiliger Zeilenkommentar
+
+/* 
+    Blockkommentar
+    erstreckt sich über 
+    mehrere Zeilen
+ */
+
+ /*
+    JS Schreibregeln
+    - case-sensitive
+        userName != Username =! username 
+    - Zählung beginnt bei 0
+    - englische Schreibweise - Dezimaltrennzeichen = . und nicht ,: 3.5 - nicht 3,5
+
+
+    Konventionen
+    - ein statement pro Zeile
+    - jede Zeile wird mit Semikolon beendet
+
+    - Bezeichner sollten sprechend sein
+
+    Regeln für Bezeichner
+    - müssen mit Buchstaben, $ oder _ beginnen
+    - ab 2. Stelle auch Zahlen erlaubt
+    - keine Sonderzeichen / Umlaute
+    - keine Leerzeichen
+    - keine reservierten Wörter
+      (https://www.w3schools.com/js/js_reserved.asp)
+    - keine Bezeichner von vordefinierten Funktionen oder Objekten
+
+    Konventionen für Bezeichner
+    - für Variablen und Funktionen: lowerCamelCase
+      abendStern, morgenStern, zwergElstern
+    
+    - constructor-Funktionen: UpperCamelCase
+
+    - Konstanten: SCREAMING_SNAKE_CASE  
+
+    Anweisungsblöcke {}
+
+ */
+
+ /*
+        vordefinierte Funktionen
+        alert("Hey Du da");
+        confirm("Bist Du sicher?"); // Rückgabe: true/false
+        prompt("Wer bist Du und wenn ja, wieviele?");
+        Rückgabe: Eingabe des Users ( evtl. "" ) oder null bei Abbruch
+ */
+
+console.log(42);
+console.log("42");
+ 
